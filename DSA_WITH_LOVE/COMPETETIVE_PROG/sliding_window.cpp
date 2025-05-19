e338d84abf801f8df174b011dda0e71c4384ea29
HAKUPA <ishnarsh1119@gmail.com>
2025-05-19 22:00:39 +0530

    updated and added sliding window techinique inside competetive programming also updated theory

---
+++ DSA_WITH_LOVE/COMPETETIVE_PROG/sliding_window.cpp
@@ -0,0 +1,95 @@
+// PROBLEM 1 :
+// GIVEN AN ARRAY FIND THE SUM OF ELEMENTS  OF THE SUBARRAY OF SIZE "K"
+// WITH MAXIMUM SUM
+
+// CAN WE OPTIMIZE THE SOLUTION  FOR ABOVE PROBLEM
+ 
+// ORIGIN OF SLIDING WINDOW PROBLEMS
+
+// PROBLEM 2:
+// FIND THE NEGATIVE NUMBER  IN EVERY WINDOW OF SIZE "K"
+
+// PROBLEM 3 :
+// GIVEN A STRING , CALCULATE THE MAXIMUM LENGTH OF A SUBSTRING IN WHICH 
+// ANY CHARACTER DOES NOT REPEAT ITSELF
+
+// TYPES OF SLIDING WINDOWS:
+// a) FIXED SIZED
+// b) VARIABLE SIZED
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define make(v)  for(auto &i:v) cin>>i
+#define prnt(v)  for(auto &i:v) cout<<i<<" "
+#define ll       long long
+
+
+// problem 1    HINT: BY DEFAULT SUBARRAY MEANS A SMALLER PART AND THAT SMALLER PART HAS TO BE CONTAGIOUS 
+//USING   B R U T E   F O R C E
+int main(){
+    int n, k;
+    cin>>n>>k;
+    int sum=0;
+    vector<int>v(n);
+    vector<int>sub_v;//(n-k+1);
+    make(v);
+    for(int i=0;i<v.size()-k+1;i++){
+        for(int j=0;j<k;j++){
+            sum=sum+v[i+j];
+        }
+        sub_v.push_back(sum);
+        sum=0;
+    }
+    auto max_ment = max_element(sub_v.begin(), sub_v.end());
+    cout << *max_ment<<endl;
+    prnt(sub_v);
+    
+    
+}
+// TEST CASES:
+// a)
+// 5 2
+// 1 3 5 2 7
+// 9
+// b)
+// 7 3
+// -3 1 3 5 -2 7 9
+// 14
+// CODE PROBLEMS
+// WHEN I WAS DOING THIS vector<int> sub_v(n - k + 1);
+// This creates a vector sub_v with (n-k+1) elements, all initialized to 0. LIKE THIS 0 0 0...(N-K+1) TIMES AND THEN AFTERWARDS THE PUSH BACKED ELEMENTS
+// Which means you're adding new elements to the end of the already allocated vector — not replacing the initial zeros.
+
+
+// 🔴 Efficiency (O(n × k))
+// In this method i was recalculating the sum from scratch for every window.
+// If n = 1e5 and k = 1e4, this becomes very slow.
+// Using sliding window to get O(n) time.
+
+// vector<int> sub_v(n - k + 1);
+// “To store the sum of every subarray of size k,
+// I realized that the first subarray starts at index 0,
+// and the last possible subarray of size k starts at index n - k.
+// So there are (n - k + 1) subarrays total.”
+
+// Example:
+// If n = 5 and k = 3,
+// Valid starting indices are i = 0, 1, 2
+// → Total = 5 - 3 + 1 = 3 subarrays.
+
+// for (int i = 0; i < v.size() - k + 1; i++) {
+// “I want to start from every index i where I can take a subarray of size k.
+// So i must go from 0 to n - k.
+// Hence the loop runs i < n - k + 1 times.”
+
+//     for (int j = 0; j < k; j++) {
+//         sum += v[i + j];
+//     }“I want the sum of elements from v[i] to v[i + k - 1],
+// so I run an inner loop for k steps and add those elements.”
+
+// CORE LOGIC "I calculated how many subarrays of size k exist by doing n - k + 1,
+// so I made a loop from 0 to n - k.
+// For each starting index i, I added up k elements from i onward.
+// I stored each of these sums in a vector and finally got the maximum."
+
--- DSA_WITH_LOVE/GREEDY/GREEDY.cpp
+++ DSA_WITH_LOVE/GREEDY/GREEDY.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define ll        long long
 
 // https://cses.fi/problemset/task/1094
-// INCREASING ARRAY
+// I N C R E A S I N G     A R R A Y
 // int main(){
 //     ll n;
 //     cin>>n;
@@ -18,6 +18,7 @@ using namespace std;
 //         continue;
 //         }else{
 //             cnt+=v[i]-v[i+1];
+//             v[i+1]+=v[i]-v[i+1];//to update the the last to last element
 //         }
 //     }
 //     cout<<cnt;
@@ -29,22 +30,24 @@ using namespace std;
 // BECAUSE THAT IS WHAT NEEDED TO MAKE TE SECOND ELEMENT EQUAL TO THE FIRST ELEMENT
 
 
+
+
 // https://cses.fi/problemset/task/1083
-//MISSING NUMBER
+//M I S S I N G   N U M B E R
 
-int main(){
-    ll n;
-    ll total_sum, sum=0;
-    cin>>n;
-    total_sum=(n*(n+1))/2;
-    vector<ll> v(n-1);
-    make(v);
-    for(ll i=0;i<n-1;i++){
-        sum+=v[i];
-    }
-    cout<<total_sum-sum;
+// int main(){
+//     ll n;
+//     ll total_sum, sum=0;
+//     cin>>n;
+//     total_sum=(n*(n+1))/2;
+//     vector<ll> v(n-1);
+//     make(v);
+//     for(ll i=0;i<n-1;i++){
+//         sum+=v[i];
+//     }
+//     cout<<total_sum-sum;
 
-}
+// }
 // LOGIC - FIRST I AM SORTING THE VECTOR SO THAT I CAN HAVE A CHECK IF THE DEFFERENCE IS 1 BETWEEN THE CONSECUTIVE NUMBERS IF IT IS NOT
 // THAT IS WHERE THE MISSING NUMBER IS SINCE IT IS NOT 1 DEFINITEKY IT HAS TO BE 2 THUS THE OUTPUT SHOULD BE ITERATION I.E "i"+"2" BUT WITH THIS LOGIUC WE'LL
 // HAVE PROBLEM WHAT IF WE ALREADY HAVE SORTED VECTOR AND THE LAST MOST DIGIT IS MISSING I.E. 
@@ -55,3 +58,99 @@ int main(){
 // CORRECT_LOGIC
 // FIND THE SUM OF ALL THE ELEMENTS USING FORMULA AS YOU KNOW THE VALUE OF "n" THEN TAKE THE SUM FROM THE VECTOR ELEMENT
 // SUBTRACT TO FIND THE MISSING NUMBER
+
+
+
+// https://cses.fi/problemset/task/1713
+// C O U N T I N G    D I V I S O R S
+
+// int main(){
+//     ll t, cnt=0;
+//     cin>>t;
+//     while(t--){
+//         ll x;
+//         cin>>x;
+//         bool check_if_perfect_sqr=0;
+//         if(floor(sqrt(x))==sqrt(x)) check_if_perfect_sqr=1;
+//         for(ll i=2;i<=sqrt(x);i++){
+//             if(x%i==0) cnt+=2; 
+//         }
+//         if(check_if_perfect_sqr) cnt--;
+//         cout<<cnt+2;
+//         cnt=0;
+//     }
+// }// with this approach my time limit is getting exceeded
+//L E A R N I N G-- TECHINQUE TO CHECK SQUARE ROOT i.e if sqrt function gives the exact value as that of floor(sqrt()) then it is a perfect square
+
+
+
+// https://codeforces.com/problemset/problem/1374/B
+// M U L T I P L Y    B Y    2    D I V I D E    B Y    6
+
+// int main(){
+//     ll t;
+//     cin>>t;
+//     while(t--){
+//         ll num, power_of2=0, power_of3=0;;
+//         cin>>num;
+//         while(num%2 == 0){
+//             power_of2++; num/=2;
+//         }
+//         while(num%3==0){
+//             power_of3++; num/=3;
+//         }
+//         if(num>1 || power_of2>power_of3){
+//         cout<<"-1"<<endl;
+//         }else{
+//             cout<<(power_of3-power_of2)+power_of3<<endl;
+//         }
+//     }
+// }
+
+
+
+// SOLVED IN CONTEST
+// int main(){
+//     ll t;
+//     cin>>t;
+//     while(t--){
+//         ll num;
+//         cin>>num;
+//         vector<ll>ar(num);
+//         make(ar);
+//         bool mark=0;
+//         bool all_1=1;
+//         for(ll i=0;i<num;i++){
+//             if(ar[i]==ar[i+1] && ar[i]==0){
+//                 mark=1;
+//                 break;
+//             }
+//         }
+//         for(ll i=0;i<num;i++){
+//             if(ar[i]==0) all_1=0;
+//         }
+//         if(mark || all_1){
+//             cout<<"yes\n";
+//         }else{
+//             cout<<"no\n";
+//         }
+//     }
+// }
+
+// https://codeforces.com/problemset/problem/1327/A
+// S U M    O F    O D D    I N T E G E R S
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        if((n%2==0 && k%2==0)&& n>=k){
+            cout<<"YES\n";
+        }else if((n%2!=0 && k%2!=0)&& n>=k){
+            cout<<"YES\n";
+        }else{
+            cout<<"NO\n";
+        }
+    }
+}
